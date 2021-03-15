dab46ee251067b2057d4968a70f102ea16bcd122
Ahmed Ibraheem <Ahmed-Ibrahim202@github.com>
2021-03-16 01:14:26 +0200

    Commit Staged

---
+++ FinalProject/Areas/User/Controllers/HemalayaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProject.Data;
+using FinalProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Areas.User.Controllers
+{
+    [Area("User")]
+    public class HemalayaController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public HemalayaController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        
+        public IActionResult Index()
+        {
+
+            var viewModel = new HomeViewModel()
+            {
+                products = context.Products.ToList(),
+                mainCategories = context.MainCategories.ToList(),
+                subCategories = context.SubCategories.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ FinalProject/Areas/User/Controllers/ShopController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProject.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Areas.User.Controllers
+{
+    [Area("User")]
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public ShopController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult ShopProducts(int SubCategoryId)
+        {
+            //check first if no id is passed 
+            if (SubCategoryId == 0) 
+            {
+                var prod = context.Products.ToList();
+                return View(prod);
+            }
+            //var subCategory = context.SubCategories.Find(SubCategoryId);
+            var products = context.Products.Where(p=>p.subCategoryId == SubCategoryId);
+
+            return View(products);
+        }
+        //filter products by price
+        public IActionResult FilterByPrice(int priceRange)
+        {
+            //get products with price less than or equal to filtering price
+            var FilteredProducts = context.Products.Where(p => p.productPrice <= priceRange).ToList();
+            return PartialView(FilteredProducts);
+        }
+
+        //product details page
+        public IActionResult ProductDetails(int productId)
+        {
+            var product = context.Products.Find(productId);
+            return View(product);
+        }
+    }
+}
